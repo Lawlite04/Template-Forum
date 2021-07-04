48f7a1233ff65f2d37bbb34cf49c7759424a8bb3
lawlite04 <thelog098@gmail.com>
2021-07-04 18:37:36 +0700

    first commit

---
+++ assets/script-navbar.js
@@ -0,0 +1,10 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const navbar = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click',function(){
+    if(menuToggle.checked == true){
+        navbar.classList.add('slide');
+    }else{
+        navbar.classList.remove('slide');
+    }
+});
